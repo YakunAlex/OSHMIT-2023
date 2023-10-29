752e085701220f35ce885fb3b13e096eebff0f34
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-29 11:03:07 +0600

    Add files via upload

---
+++ 1hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+// магия чтобы все ззаработало
+
+int main() {
+	// вот тут сама программа
+	
+	cout << "Hello world" << endl;
+	// вывод строчки Hello world
+	// endl - это перенос строки
+	
+	cout << "Hello shmit" << endl;
+}
