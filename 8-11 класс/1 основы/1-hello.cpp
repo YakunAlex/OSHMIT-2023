77458fed52010b0d369bff906ac1bd20c413a3b4
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-30 00:27:17 +0600

    Add files via upload

---
+++ 8-11 класс/1 основы/1-hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+// магия чтобы все работало
+
+int main() {
+	// вот тут будет вся программа
+	
+	cout << "Hello world!" << endl;
+	// вывод строчки hello world
+	
+	cout << "Hi Shmit!" << endl;
+	
+	cout << "1 + 2" << endl;
+	
+	cout << 1 + 2 << endl;
+}
---
+++ 8-11 класс/1 основы/3-operations.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	// считали переменную x
+	
+	cout << x + y << endl;
+	cout << x - y << endl;
+	cout << x * y << endl;
+	// сложение, вычитание, умножение
+	cout << x / y << endl;
+	// целочисленное деление (округляя вниз)
+	cout << x % y << endl;
+	// остаток по модулю
+}
