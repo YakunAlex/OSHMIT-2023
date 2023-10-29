e5287b7e43f446dcec620c3f7def91910f3e0576
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-30 00:22:24 +0600

    Create 3-operations.cpp

---
+++ 7 класс/1 основы/3-operations.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	
+	cout << x + y << endl;
+	// ñëîæåíèå
+	
+	cout << x - y << endl;
+	// âû÷èòàåíèå
+	
+	cout << x*y << endl;
+	// óìíîæåíèå
+	
+	cout << x/y << endl;
+	// öåëî÷èñëåííîå äåëåíèå (ñ îòáðàñûâàíèåì îñòàòêà)
+	
+	cout << x%y << endl;
+	// âçÿòèå îñòàòêà
+}
