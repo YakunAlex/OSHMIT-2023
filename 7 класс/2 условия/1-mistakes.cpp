25ddda21c45e44801119044b155bc18fd99aa885
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-30 15:26:49 +0600

    Create 1-mistakes.cpp

---
+++ 7 класс/2 условия/1-mistakes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+
+	// cout << x;
+	// может быть что угодно
+
+	// ЧАСТАЯ ОШИБКА
+	cin >> x, y;
+	// считается только x
+	// в y будет что угодно
+}
