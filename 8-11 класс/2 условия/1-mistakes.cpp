cc536108477f918972832d11e59c8315414e2bb6
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-30 10:47:11 +0600

    Create 1-mistakes.cpp

---
+++ 8-11 класс/2 условия/1-mistakes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cout << x << endl;
+	// может быть выведено что угодно
+	
+	int y, z;
+	cin >> y, z;
+	// считывается только y
+	cout << y << " " << z << endl;э
+	// во втором числе может быть что угодно
+
+}
