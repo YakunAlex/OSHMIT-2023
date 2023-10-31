d59d9a999bb213d93361afcbdcc239b22ac1f55e
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-10-31 15:09:11 +0600

    Create 1-tricks.cpp

---
+++ 8-11 класс/3 циклы/1-tricks.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+
+	x = x + 1;
+	cout << x << endl;
+
+	// x += a увеличивает x на a
+	x += 1;
+	cout << x << endl;
+
+	// x += 1
+	x++;
+	cout << x << endl;
+}
