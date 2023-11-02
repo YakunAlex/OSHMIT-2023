95c8f2387414a10efe958f2bb114cd73e15ebe80
YakunAlex <34012398+YakunAlex@users.noreply.github.com>
2023-11-02 11:28:17 +0600

    Create 3-search-v2.cpp

---
+++ 7 класс/4 массивы/3-search-v2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+// нужно подключить эту библиотеку
+#include <vector>
+using namespace std;
+
+int main() {
+	// задача: дано n, потом n чисел a_1...a_n и число x
+	// требуется сказать, встречается ли x среди a_1...a_n 
+	int n;
+	cin >> n;
+
+	// мы объявили переменную a типа "массив"
+	// массив (только вектор) автоматически заполняется нулями
+	vector<int> a(n);
+
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+
+	int x;
+	cin >> x;
+	bool ok = false;
+
+	for (int i = 0; i < n; ++i) {
+		if (x == a[i]) {
+			cout << "yes" << endl;
+			ok = true;
+			break;
+		}
+	}
+
+	// ! - это логическое отрицание
+	if (!ok) {
+		cout << "no" << endl;
+	}	
+}
